0ef38ecbf48c253f89d6f6dd1c4ad3a70202d6cb
JozoP <jozo035@gmail.com>
2024-06-27 21:56:12 +0200

    Packages

---
+++ base_external/package/aesd-char/aesd-char.mk
@@ -0,0 +1,23 @@
+AESD_CHAR_VERSION = 'b3c1e9416d5a0457a488358cbe83898f583c50d4'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-JozoP.git
+AESD_CHAR_SITE_METHOD = git
+AESD_CHAR_GIT_SUBMODULES = YES
+
+AESD_CHAR_MODULE_SUBDIRS = aesd-char-driver/
+
+# TODO adding all the dependencies.
+define AESD_CHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
+
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
