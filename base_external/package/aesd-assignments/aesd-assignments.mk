bedeecf35de151136eab7d1f1d5511833c64430f
JozoP <jozo035@gmail.com>
2024-06-30 11:41:05 +0200

    typos

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '188016caaf8da91fa4b31364e81d5c2d36d22ff6'
+AESD_ASSIGNMENTS_VERSION = '6400f9ef5a03c90747a823a7fd8d58dfe6446ad6'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
